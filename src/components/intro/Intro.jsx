162b4fa1cf2343245ce52df82bd02a86a5020be4
kerkpy <peiyongkerk@gmail.com>
2021-07-22 21:37:02 +0800

    Introduction

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,47 @@
 import "./intro.scss"
+import { init } from 'ityped'
+import { useEffect, useRef } from "react";
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default function Intro() {
+
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            showCursor:true,
+            disableBackTyping:true,
+            typeSpeed: 50,
+            strings: ["A NUS CS Student Passionate about Tech."],
+        });
+
+    }, []);
+
+        
     return (
         <div className="intro" id = "intro">
-            Intro!
+            <div className="left">
+                <div className="imgContainer">
+                    <img src = "assets/me.jfif" alt= "Me!"></img>
+                </div>
+            </div>
+
+
+
+            <div className="right">
+                <div className = "wrapper">
+                    <h2>Hi There! I am</h2>
+                    <h1>Kerk Pei Yong</h1>
+                    <h3><span ref={textRef}></span></h3>
+
+                    <a href="#portfolio">
+                    <ExpandMoreIcon/>
+                
+                    </a>
+                </div>
+
+                
+            </div>
         </div>
     )
 }
